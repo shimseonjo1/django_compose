1a91f99e6f743d555cd1894ddd63128b5e99a5a8
shimseonjo1 <shimseonjo@naver.com>
2023-06-16 12:30:19 +0900

    basic page url add

--- django_prj/urls.py
+++ django_prj/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+     path('/', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
